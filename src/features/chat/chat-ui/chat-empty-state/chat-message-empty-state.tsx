d143194a011950040295bb4e546c4604c05dea85
chatgptkwb <kashiwabarachatgpt@gmail.com>
2025-01-06 09:36:27 +0900

    メッセージ変更

--- src/features/chat/chat-ui/chat-empty-state/chat-message-empty-state.tsx
+++ src/features/chat/chat-ui/chat-empty-state/chat-message-empty-state.tsx
@@ -20,15 +20,6 @@ export const ChatMessageEmptyState: FC<Prop> = (props) => {
   return (
     <div className="grid grid-cols-1 w-full items-center container mx-auto max-w-4xl justify-center h-full gap-9">
       <Card className="col-span-3 flex flex-col gap-5 p-5 ">
-        <Typography variant="h4" className="text-primary">
-        {process.env.NEXT_PUBLIC_AI_NAME}
-        </Typography>
-          <p className="text-xs text-muted-foreground">
-            {process.env.NEXT_PUBLIC_AI_WELCOME_MSG_JA}
-            ご利用の際は、以下の項目を選択してください。
-
-          </p>
-
 
         <div className="flex flex-col gap-2">
           <p className="text-sm text-muted-foreground">
